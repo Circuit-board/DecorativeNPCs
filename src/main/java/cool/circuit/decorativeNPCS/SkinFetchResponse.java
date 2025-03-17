63a362d85b98de996b9ebdf0fd2ebe68a9c6d371
Circuit-board <153775718+Circuit-board@users.noreply.github.com>
2025-03-17 12:33:49 -0400

    Delete src/main/java/cool/circuit directory

--- src/main/java/cool/circuit/decorativeNPCS/SkinFetchResponse.java
+++
@@ -1,9 +0,0 @@
-package cool.circuit.decorativeNPCS;
-
-public enum SkinFetchResponse {
-    SUCCESS,
-    FAILED_TO_FETCH_SKIN,
-    DATA_IS_NULL,
-    SKIN_VALUE_OR_SIGNATURE_IS_NULL,
-    NO_SERVER_PLAYER
-}
